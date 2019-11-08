5fe512287840c30c196040f49918d3c4e17c299a
Brent Groves <brent.groves@gmail.com>
2019-11-08 09:54:55 -0500

    Added record production

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const feathers = require('@feathersjs/feathers');
 const socketio = require('@feathersjs/socketio');
-
+const mqtt = require('mqtt');
 
 // A messages service that allows to create new
 // and return all existing messages
@@ -11,18 +11,18 @@ class MessageService {
     this.messages = [];
   }
 
-  async find () {
+  async find() {
     // Just return all our messages
     return this.messages;
   }
 
-  async create (data) {
+  async create(data) {
     // The new message is the data merged with a unique identifier
     // using the messages length since it changes whenever we add one
     const message = {
       id: this.messages.length,
-      text: data.text
-    }
+      text: data.text,
+    };
 
     // Add new message to the list
     this.messages.push(message);
@@ -41,21 +41,41 @@ app.use('/messages', new MessageService());
 //app.use(express.errorHandler());
 
 // Add any new real-time connection to the `everybody` channel
-app.on('connection', connection =>
-  
-  app.channel('everybody').join(connection)
-);
+app.on('connection', connection => app.channel('everybody').join(connection));
 // Publish all events to the `everybody` channel
 app.publish(data => app.channel('everybody'));
 
 // Start the server
-app.listen(3030).on('listening', () =>
-  console.log('Feathers server listening on localhost:3030')
-);
+app
+  .listen(3030)
+  .on('listening', () =>
+    console.log('Feathers server listening on localhost:3030'),
+  );
 
 // For good measure let's create a message
 // So our API doesn't look so empty
 app.service('messages').create({
-  text: 'Hello world from the server'
+  text: 'Hello world from the server',
 });
 
+let mqttClient = mqtt.connect(
+  'mqtt://ec2-3-15-151-115.us-east-2.compute.amazonaws.com',
+  // 'mqtt://test.mosquitto.org'
+  // 'mqtt://localhost',
+);
+
+mqttClient.on('connect', function() {
+  mqttClient.subscribe('Plex13318', function(err) {
+    if (!err) {
+      console.log('subscribed to: Plex13318');
+    }
+  });
+});
+// message is a buffer
+mqttClient.on('message', function(topic, message) {
+  const params = JSON.parse(message.toString()); // payload is a buffer
+  console.log(params);
+  app.service('messages').create({
+    text: 'mqtt message',
+  });
+});
